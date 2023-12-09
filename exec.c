86c5f638893d2f0d61d72127a783c776d3df54c0
Kamal Abdelmalek <144700532+KamalCode98@users.noreply.github.com>
2023-12-10 00:29:53 +0100

    updates

--- exec.c
+++ exec.c
@@ -12,8 +12,18 @@ int _execute(char **command, char **argv)
     child_process = fork();
     if (child_process == 0)
     {
-        if (execve(command[0], command, environ))
-        
+        if (execve(command[0], command, environ) == -1);
+        {
+            perror(argv[0]);
+            free_arrguements(command);
+        }
     }
+    else
+    {
+        waitpid(child_process, &status, 0);
+        free_arrguements(command);
+
+    }
+
 
 }
