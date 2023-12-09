fd1775bdc69f5faee0335d0bcf7781ec74612982
Kamal Abdelmalek <144700532+KamalCode98@users.noreply.github.com>
2023-12-09 20:52:48 +0100

    "tests"

--- main.c
+++ main.c
@@ -14,6 +14,7 @@ int main(int ac, char **argv)
 	char *line = NULL;
 	char **command = NULL;
 	int status;
+	int i;
 	(void) ac;
 	(void)argv;
 
@@ -32,7 +33,7 @@ int main(int ac, char **argv)
 		if (!command)
 			continue;
 
-		for (i = 0, command[i]; i++)
+		for (i = 0; command[i]; i++)
 			printf("%s\n", command[i]);
 
 
